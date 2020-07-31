b651c8f33a96beda6b6e4f46ba63f4700f524634
max198725 <68507837+max198725@users.noreply.github.com>
2020-07-31 15:48:31 +0300

    Update Mine.py

--- Mine.py
+++ Mine.py
@@ -1,11 +1,15 @@
 from colorama import Fore, Back, Style
 import os
+type1 = type(a)
+type2 = type(b)
 while True:
     os.system("clear")
     print(Fore.BLUE + "дЭбылный кулькулятор v1")
     print("")
-    a = float(input("Введите первое число: "))
-    b = float(input("Введите второе число: "))
+    a = input("Введите первое число: ")
+    if type1 == str:
+        print("Попробуй ещё раз, видимо у тебя была буква в числе")
+    b = input("Введите второе число: ")
     print("")
     d = input("Что вы хотите сделать? +, -, *, /")
     if d == "+":
