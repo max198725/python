26c38c961f5ab7e23b47d8b3d435a3f1fe497877
max198725 <68507837+max198725@users.noreply.github.com>
2020-07-31 15:39:36 +0300

    Update Mine.py

--- Mine.py
+++ Mine.py
@@ -1,12 +1,18 @@
 from colorama import Fore, Back, Style
 import os
-os.system("clear")
-print(Fore.BLUE + "дЭбылный кулькулятор v1")
-print("")
-a = float(input("Введите первое число: "))
-b = float(input("Введите второе число: "))
-print("")
-c = a + b
-print(c)
+while True:
+    os.system("clear")
+    print(Fore.BLUE + "дЭбылный кулькулятор v1")
+    print("")
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    print("")
+    d = input("Что вы хотите сделать? +, -, *, /")
+    if d == "+":
+        c = a + b
+    elif d == "-":
+        c = a - b
+
+    print("Ответ:", c)
 print(Fore.RESET + "")
 
