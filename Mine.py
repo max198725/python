1e361de4ad7227391799c7b690682d1add0a6e6c
max198725 <68507837+max198725@users.noreply.github.com>
2020-07-31 15:20:51 +0300

    Update Mine.py

--- Mine.py
+++ Mine.py
@@ -1,8 +1,7 @@
 from colorama import Fore, Back, Style
 import os
 os.system("clear")
-Back.BLUE
-print("дЭбылный кулькулятор v1")
+print(Back.BLUE + "дЭбылный кулькулятор v1")
 print("")
 a = float(input("Введите первое число: "))
 b = float(input("Введите второе число: "))
