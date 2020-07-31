36704ade706b7d0f3ab81f8c1b594d08b1ade361
max198725 <68507837+max198725@users.noreply.github.com>
2020-07-31 23:42:05 +0300

    Update Mine.py

--- Mine.py
+++ Mine.py
@@ -1,22 +1,51 @@
 from colorama import Fore, Back, Style
 import os
-type1 = type(a)
-type2 = type(b)
-while True:
-    os.system("clear")
-    print(Fore.BLUE + "дЭбылный кулькулятор v1")
-    print("")
-    a = input("Введите первое число: ")
-    if type1 == str:
-        print("Попробуй ещё раз, видимо у тебя была буква в числе")
-    b = input("Введите второе число: ")
-    print("")
-    d = input("Что вы хотите сделать? +, -, *, /")
-    if d == "+":
-        c = a + b
-    elif d == "-":
-        c = a - b
-
-    print("Ответ:", c)
-print(Fore.RESET + "")
-
+os.system("clear")
+print(Fore.BLUE + "дЭбылный кулькулятор v1")
+print("")
+i = 0
+while i < 10:
+	while True:
+		try:
+			a = float(input("Введите первое число: "))
+			break
+		except ValueError:
+			print("Попробуй ещё раз, видимо у тебя была буква в числе")
+	while True:
+			try:
+				b = float(input("Введите второе число: "))
+				break
+			except ValueError:
+				print("Попробуй ещё раз, видимо у тебя была буква в числе")
+	while True:
+			d = input("Что ты хочешь сделать? +, -, *, /: ")
+			try:
+				if d == "+":
+					c = a + b
+					break
+				elif d == "-":
+					c = a - b
+					break
+				elif d == "*":
+					c = a * b
+					break
+				elif d == "/":
+					c = a / b
+					break
+				else:
+					print("Ты невернно ввёл действие, попробуй ещё раз")
+					continue
+			except ZeroDivisionError:
+				print("Дружище, на 0 делить нельзя, попробуй ещё раз")
+				continue
+	while True:
+				print("Ответ:", c)
+				break
+	while i < 10:
+				m = input(("Хотите ещё раз? Да или нет: "))
+				if m == "Нет":
+					print(Fore.RESET)
+					i = 11
+				elif m == "Да":
+					os.system("clear")
+					break
